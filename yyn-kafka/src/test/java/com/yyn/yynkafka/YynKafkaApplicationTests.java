a7767577954e1a818ccb21a3369eb013a3c23d9e
yynlove <1993208827@qq.com>
2024-10-28 21:12:42 +0800

    1、kafka

---
+++ yyn-kafka/src/test/java/com/yyn/yynkafka/YynKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyn.yynkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YynKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
